00bb6ab700c047a1d99594263e9c645ca7b89da9
plutownium <rolandmackintosh@gmail.com>
2020-03-28 17:12:00 -0700

    cleaning up unused code, adding solution

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,54 @@
 // Grab the elements on the page.
-let button = document.querySelector("#toggleButton");
-let slide = document.querySelector(".slide");
+// let button = document.querySelector("#toggleButton");
+// let slide = document.querySelector(".slide");
 
-// Add event listeners.
-button.addEventListener(
-	"click",
+// // Add event listeners.
+// button.addEventListener(
+// 	"click",
+// 	() => {
+// 		slide.setAttribute("class", "slide slideIn");
+// 	},
+// 	false
+// );
+
+// slide.addEventListener(
+// 	"click",
+// 	() => {
+// 		slide.setAttribute("class", "slide slideOut");
+// 	},
+// 	false
+// );
+
+// let button2 = document.querySelector("#anotherToggle");
+// let slide2 = document.querySelector(".test-div");
+
+// button2.addEventListener(
+// 	"click",
+// 	() => {
+// 		slide2.setAttribute("class", ".test-div s-in");
+// 	},
+// 	false
+// );
+
+// slide2.addEventListener(
+// 	"click",
+// 	() => {
+// 		slide2.setAttribute("class", "test-div s-out");
+// 	},
+// 	false
+// );
+
+// ~*~*~*~*~*~**~*~*~
+
+let blackStuff = document.getElementById("blackness");
+
+window.addEventListener(
+	"load",
 	() => {
-		slide.setAttribute("class", "slide slideIn");
+		console.log("loaded");
+		blackStuff.setAttribute("class", "black-box-out");
 	},
 	false
 );
 
-slide.addEventListener(
-	"click",
-	() => {
-		slide.setAttribute("class", "slide slideOut");
-	},
-	false
-);
+// *~*~*~*~*~*~*~*~
---
+++ testing/testScript.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+	let animate = document.querySelectorAll(".entrance-animation");
+
+	animate.forEach(item => item.classList.add("active"));
+}, 1000);
