876f5cd191f7abb06049f141491a2beb605f7916
plutownium <rolandmackintosh@gmail.com>
2020-04-06 15:27:22 -0700

    removed code for unused blackness div

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,1 @@
-let blackStuff = document.getElementById("blackness");
-
-window.addEventListener(
-	"load",
-	() => {
-		console.log("loaded");
-		blackStuff.setAttribute("class", "black-box-out");
-	},
-	false
-);
-
-// *~*~*~*~*~*~*~*~
-
-// TODO: mk bg img resize to different devices (use media query?)
-// todo: mk bg img darken as scroll gets lower
-
-// FIXME: 5ish px white border around top and left side of page
+console.log("Loaded!");
--- testing/testScript.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-	let animate = document.querySelectorAll(".entrance-animation");
-
-	animate.forEach(item => item.classList.add("active"));
-}, 1000);
